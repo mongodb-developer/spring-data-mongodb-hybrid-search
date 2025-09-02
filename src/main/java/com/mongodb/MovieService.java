6869653d9cb9e9908157f0b45f5744409d2dc1c8
Ricardo Mello <ricardohsmello@gmail.com>
2025-09-02 16:57:44 -0300

    Refactor: including hybrid search

--- src/main/java/com/mongodb/MovieService.java
+++ src/main/java/com/mongodb/MovieService.java
@@ -1,13 +1,21 @@
 package com.mongodb;
 
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.search.FuzzySearchOptions;
+import com.mongodb.client.model.search.SearchOperator;
+import com.mongodb.client.model.search.SearchOptions;
+import com.mongodb.client.model.search.SearchPath;
+import org.bson.BsonDocument;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.VectorSearchOperation;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
 @Service
 @EnableConfigurationProperties(VoyageConfigProperties.class)
@@ -23,16 +31,40 @@ public class MovieService {
 		this.embeddingService = embeddingService;
 	}
 
-	public List<Movie> searchMovies(MovieSearchRequest req) {
-		VectorSearchOperation search = VectorSearchOperation.search(config.vectorIndexName())
+	private BsonDocument createSearch(String query) {
+		return Aggregates.search(
+				SearchOperator.text(
+						SearchPath.fieldPath("title"),
+						query
+				).fuzzy(FuzzySearchOptions.fuzzySearchOptions().maxEdits(2)),
+				SearchOptions.searchOptions().index("fulltextsearch")
+		).toBsonDocument();
+	}
+
+	private Bson createVectorSearch(MovieSearchRequest req) {
+		return VectorSearchOperation.search(config.vectorIndexName())
 				.path(config.vectorField())
 				.vector(embeddingService.embedQuery(req.query()))
 				.limit(config.topK())
 				.filter(req.toCriteria())
 				.numCandidates(config.numCandidates())
-				.withSearchScore("score");
+				.withSearchScore("score").toDocument(Aggregation.DEFAULT_CONTEXT);
+	}
 
-		return mongoTemplate.aggregate(newAggregation(Movie.class, search), Movie.class)
-				.getMappedResults();
+	public List<Movie> searchMovies(MovieSearchRequest req) {
+		AggregationOperation rankFusion = context -> new Document("$rankFusion",
+				new Document("input",
+						new Document("pipelines",
+								new Document("searchPipeline", List.of(createSearch(req.query())))
+										.append("vectorPipeline", List.of(createVectorSearch(req)))))
+						.append("combination",
+								new Document("weights",
+										new Document("searchPipeline", 0.1d)
+												.append("vectorPipeline", 0.9d)))
+						.append("scoreDetails", false));
+
+		Aggregation aggregation = Aggregation.newAggregation(rankFusion);
+
+		return mongoTemplate.aggregate(aggregation, "embedded_movies", Movie.class).getMappedResults();
 	}
 }
