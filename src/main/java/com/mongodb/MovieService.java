fb2e4c52f51040bd9108e4b68c15e5f6da78d18e
Ricardo Mello <ricardohsmello@gmail.com>
2025-09-05 17:57:05 -0300

    Refactor: including boost option for atlas search

--- src/main/java/com/mongodb/MovieService.java
+++ src/main/java/com/mongodb/MovieService.java
@@ -1,8 +1,6 @@
 package com.mongodb;
 
 import com.mongodb.client.model.Aggregates;
-import com.mongodb.client.model.search.FuzzySearchOptions;
-import com.mongodb.client.model.search.SearchOperator;
 import com.mongodb.client.model.search.SearchOptions;
 import com.mongodb.client.model.search.SearchPath;
 import org.bson.BsonDocument;
@@ -17,9 +15,13 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.mongodb.client.model.search.SearchOperator.*;
+import static com.mongodb.client.model.search.SearchScore.*;
+
 @Service
 @EnableConfigurationProperties(VoyageConfigProperties.class)
 public class MovieService {
+
 
 	private final MongoTemplate mongoTemplate;
 	private final VoyageConfigProperties config;
@@ -33,10 +35,16 @@ public class MovieService {
 
 	private BsonDocument createSearch(String query) {
 		return Aggregates.search(
-				SearchOperator.text(
-						SearchPath.fieldPath("title"),
-						query
-				).fuzzy(FuzzySearchOptions.fuzzySearchOptions().maxEdits(2)),
+				compound().should(
+						List.of(
+								text(SearchPath.fieldPath("title"), query)
+										.score(boost(4.0F)),
+								text(SearchPath.fieldPath("plot"), query)
+										.score(boost(3.0F)),
+								text(SearchPath.fieldPath("fullplot"), query)
+										.score(boost(2.0F))
+						)
+				),
 				SearchOptions.searchOptions().index("fulltextsearch")
 		).toBsonDocument();
 	}
@@ -55,12 +63,12 @@ public class MovieService {
 		AggregationOperation rankFusion = context -> new Document("$rankFusion",
 				new Document("input",
 						new Document("pipelines",
-								new Document("searchPipeline", List.of(createSearch(req.query())))
+								new Document("searchPipeline", List.of(createSearch(req.query()), new Document("$limit", config.topK())))
 										.append("vectorPipeline", List.of(createVectorSearch(req)))))
 						.append("combination",
 								new Document("weights",
-										new Document("searchPipeline", 0.1d)
-												.append("vectorPipeline", 0.9d)))
+										new Document("searchPipeline", 0.9)
+												.append("vectorPipeline", 0.1)))
 						.append("scoreDetails", false));
 
 		Aggregation aggregation = Aggregation.newAggregation(rankFusion);
