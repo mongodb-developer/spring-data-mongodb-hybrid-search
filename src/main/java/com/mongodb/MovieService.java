57d316b86d3e2605394975bab45c8c3b4822dfac
Ricardo Mello <ricardohsmello@gmail.com>
2025-09-08 16:28:03 -0300

    Refactor: including boost option for atlas search

--- src/main/java/com/mongodb/MovieService.java
+++ src/main/java/com/mongodb/MovieService.java
@@ -1,6 +1,7 @@
 package com.mongodb;
 
 import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.search.SearchOperator;
 import com.mongodb.client.model.search.SearchOptions;
 import com.mongodb.client.model.search.SearchPath;
 import org.bson.BsonDocument;
@@ -13,7 +14,10 @@ import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.VectorSearchOperation;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.mongodb.client.model.search.FuzzySearchOptions.*;
 import static com.mongodb.client.model.search.SearchOperator.*;
@@ -34,49 +38,79 @@ public class MovieService {
 		this.embeddingService = embeddingService;
 	}
 
-	private BsonDocument createSearch(String query) {
+	private BsonDocument createFullTextSearchPipeline(MovieSearchRequest req) {
 		return Aggregates.search(
-				compound().should(
-						List.of(
-								text(SearchPath.fieldPath("title"), query)
-										.fuzzy(fuzzySearchOptions().maxEdits(1))
-										.score(boost(4.0F)),
-								text(SearchPath.fieldPath("plot"), query)
-										.fuzzy(fuzzySearchOptions().maxEdits(1))
-										.score(boost(3.0F)),
-								text(SearchPath.fieldPath("fullplot"), query)
-										.fuzzy(fuzzySearchOptions().maxEdits(1))
-										.score(boost(2.0F))
-						)
-				),
+				compound().filter(buildFilters(req)).should(buildSearchClauses(req)),
 				SearchOptions.searchOptions().index("fulltextsearch")
 		).toBsonDocument();
 	}
 
-	private Bson createVectorSearch(MovieSearchRequest req) {
+	private List<SearchOperator> buildFilters(MovieSearchRequest req) {
+		var filters = new ArrayList<SearchOperator>();
+
+		filters.add(
+				text(SearchPath.fieldPath("title"), req.query())
+						.score(boost(4.0F))
+						.fuzzy(fuzzySearchOptions().maxEdits(1)));
+
+		if (req.genres() != null && !req.genres().isEmpty()) {
+			filters.add(in(SearchPath.fieldPath("genres"), req.genres()));
+		}
+
+		if (req.yearFrom() != null) {
+			filters.add(numberRange(SearchPath.fieldPath("year")).gte(req.yearFrom()));
+		}
+
+		if (req.yearTo() != null) {
+			filters.add(numberRange(SearchPath.fieldPath("year")).lte(req.yearTo()));
+		}
+
+		if (req.minIMDbRating() != null) {
+			filters.add(numberRange(SearchPath.fieldPath("imdb.rating")).gte(req.minIMDbRating()));
+		}
+
+		return filters;
+	}
+
+	private List<SearchOperator> buildSearchClauses(MovieSearchRequest req) {
+		Map<String, Float> fieldConfigs = Map.of(
+				"plot", 3.0F,
+				"fullplot", 2.0F
+		);
+
+		return fieldConfigs.entrySet().stream()
+				.map(entry -> text(SearchPath.fieldPath(entry.getKey()), req.query())
+						.fuzzy(fuzzySearchOptions().maxEdits(1))
+						.score(boost(entry.getValue())))
+				.collect(Collectors.toList());
+	}
+
+	private Bson createVectorSearchPipeline(MovieSearchRequest req) {
 		return VectorSearchOperation.search(config.vectorIndexName())
 				.path(config.vectorField())
 				.vector(embeddingService.embedQuery(req.query()))
 				.limit(config.topK())
 				.filter(req.toCriteria())
-				.numCandidates(config.numCandidates())
-				.withSearchScore("score").toDocument(Aggregation.DEFAULT_CONTEXT);
+				.numCandidates(config.numCandidates()).toDocument(Aggregation.DEFAULT_CONTEXT);
 	}
 
 	public List<Movie> searchMovies(MovieSearchRequest req) {
+
 		AggregationOperation rankFusion = context -> new Document("$rankFusion",
 				new Document("input",
 						new Document("pipelines",
-								new Document("searchPipeline", List.of(createSearch(req.query()), new Document("$limit", config.topK())))
-										.append("vectorPipeline", List.of(createVectorSearch(req)))))
+								new Document("searchPipeline", List.of(createFullTextSearchPipeline(req), new Document("$limit", config.topK())))
+										.append("vectorPipeline", List.of(createVectorSearchPipeline(req)))))
 						.append("combination",
 								new Document("weights",
-										new Document("searchPipeline", 0.9)
-												.append("vectorPipeline", 0.1)))
+										new Document("searchPipeline", 0.2)
+												.append("vectorPipeline", 0.8)))
 						.append("scoreDetails", false));
 
 		Aggregation aggregation = Aggregation.newAggregation(rankFusion);
 
 		return mongoTemplate.aggregate(aggregation, "embedded_movies", Movie.class).getMappedResults();
 	}
+
+
 }
